8d5fb85df15ef337f7700a96896938d9a3b54977
Quentin DAVIN <84854854+itsax404@users.noreply.github.com>
2025-05-16 15:03:32 +0000

    Bump version: 0.1.1-dev0 → 0.1.2-dev0

--- mkdocs_protobuf_plugin/__init__.py
+++ mkdocs_protobuf_plugin/__init__.py
@@ -3,4 +3,4 @@ from .plugin import ProtobufPlugin  # noqa: F401
 from .file_cache import ProtoFileCache  # noqa: F401
 from .i18n_support import I18nSupport  # noqa: F401
 
-__version__ = '0.1.1-dev0'  # Will be updated by bumpversion
+__version__ = '0.1.2-dev0'  # Will be updated by bumpversion
